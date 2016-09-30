0d334a65071abfb7cae361ef5683b9c5effebe60
Edmundo Pichardo <ce.pichardo@gmail.com>
2016-09-30 17:13:28 -0400

    Se arreglo l apaginacion en CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page:5)
   end
 
   def new
